66c7121128d42cc20f4f1c3865cd4442291a4157
rjbranch <rjbrjb@umich.edu>
2016-03-01 15:27:39 -0500

    Google search parsing
    The program now takes a string, instead of a list of URLs.  It uses this
    string to search Google, and the URLs used are retrieved from those
    results.

--- grab.py
+++ grab.py
@@ -12,6 +12,7 @@ class Container:
 	def __init__(self):
 		self.fileName = "default.txt"
 		self.folderName = "default_txt"
+		self.searchTerms = []
 		self.urls = []
 		self.extensions = []
 		self.links = []
@@ -40,6 +41,18 @@ class Container:
 	#Get function for folderName
 	def getFolderName(self):
 		return self.folderName
+
+	#Function to add a string to the end of "searchTerms"
+	def addSearchTerm(self, termIn):
+		self.searchTerms.append(termIn)
+
+	#Function to return the search term at a given index of "searchTerms"
+	def getSearchTerm(self, index):
+		return self.searchTerms[index]
+
+	#Function to return the entire "searchTerms" list
+	def getSearchTerms(self):
+		return self.searchTerms
 
 	#Function to add a string to the end of "urls"
 	def addUrl(self, urlIn):
@@ -85,32 +98,33 @@ class Container:
 	def getRowNum(self):
 		return self.rowNum
 
-def getLinks(url, cont):
+def getLinks(cont, i):
+	url = cont.getUrl(i)
 	try:
 		response = urllib2.urlopen(url)
 	except:
 		print('Cannot read HTML: ' + url)
 		return False
-	response = urllib2.urlopen(url)
 	html = response.read()
 	content = StringIO(html)
 	for line in content:
 		for ext in cont.getExtensions():
 			if ext in line:
 				try:
-					link = re.search("href=\"(.+?)\"", line).group(1)
-					link = url + link
-					print(link)
+					link = re.search("href=\"(.+?)" + ext + "\"", line).group(1)
+					link = url + link + ext
 					cont.addLink(link)
+					break
 				except:
-					print("Could not find link with extension: " + ext + " in line " + line)
-
+					#This often occurs if a link is preceded by an icon,
+					#for example "sound2.gif" preceding a .mp3 file.
+					pass
 
 def checkUrl(url):
 	try:
 		response = urllib2.urlopen(url)
 	except:
-		print("Cannot read HTML: " + url)
+		print("Cannot parse HTML: " + url)
 		return False
 	response = urllib2.urlopen(url)
 	html = response.read()
@@ -131,12 +145,47 @@ def fileInput(cont):
 			for col in row:
 				cont.addExtension(col)
 			cont.setRowNum(cont.getRowNum() + 1)
-		#Gets URL data
+		#Gets search term data
 		else:
-			cont.addUrl(row[0])
+			cont.addSearchTerm(row[0])
 
 	#All objects are now read in
 	data.close
+
+def parseGoogle(cont, url):
+	print("CANDIDATES:")
+	try:
+		opener = urllib2.build_opener()
+		opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36')]
+		response = opener.open(url)
+	except:
+		print('Cannot read Google Search HTML: ' + url)
+		return False
+	html = response.read()
+	try:
+		links = re.findall("href=\"http(.+?)\"", html)
+	except:
+		print("Could not retrieve links from search results page.")
+	for link in links:
+		if "google" not in link:
+			link = "http" + link
+			if (link[-1] != '/'):
+				link = link + '/'
+			print(link)
+			cont.addUrl(link)
+
+def buildURLs(cont):
+	for phrase in cont.getSearchTerms():
+		parseGoogle(cont, getSearchURL(phrase, 1))
+
+def getSearchURL(term, pageNum):
+	tempName = ""
+	for char in term:
+		if (char == ' '):
+			tempName += "+"
+		else:
+			tempName += char
+	return "https://www.google.com/search?q=" + tempName + "&start=" + str((pageNum - 1) * 10)
 
 def download(cont):
 	for link in cont.getLinks():
@@ -163,6 +212,7 @@ def main():
 		container.setFolderName(tempName)
 
 	fileInput(container)
+	buildURLs(container)
 
 	successes = []
 	for url in container.getUrls():
@@ -174,7 +224,7 @@ def main():
 	index = 0
 	for val in successes:
 		if val:
-			getLinks(container.getUrl(index), container)
+			getLinks(container, index)
 		index += 1
 
 	download(container)
